4b8c05f561dc446650f92487e7b7646de58eaa11
Asad Khan <asad@orglens.com>
2023-02-04 13:05:16 +0530

    Faviicon added

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="./favicon.png" />
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon.ico" />
     <?php wp_head(); ?>
     <?php seo_mete_data(); ?>
 
